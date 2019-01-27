6b5c6ac203fe045eb892f7ac7806bc8230f2318b
Vincent Link <mail@linkvt.de>
2019-01-27 23:57:48 +0100

    Extract CSS into separate file in production build

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 * {
   font-family: "Lato", sans-serif;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,56 @@
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-module.exports = {
-  resolve: {
-    extensions: [".js", ".json", ".vue"]
-  },
-  module: {
-    rules: [{
-        test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"]
-      },
-      {
-        test: /\.scss$/,
-        use: ["vue-style-loader", "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: "html-loader",
-          options: {
-            minimize: true
+
+module.exports = (env, argv) => {
+  const styleLoader = argv.mode !== "production" ? "vue-style-loader" : MiniCssExtractPlugin.loader;
+
+  return {
+    resolve: {
+      extensions: [".js", ".json", ".vue"]
+    },
+    module: {
+      rules: [{
+          test: /\.css$/,
+          use: [styleLoader, "css-loader"]
+        }, {
+          test: /\.scss$/,
+          use: [styleLoader, "css-loader", "sass-loader"]
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
           }
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [{
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
+          }]
+        },
+        {
+          test: /\.vue$/,
+          use: ["vue-loader"]
         }
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [{
-          loader: "url-loader",
-          options: {
-            limit: 8192
-          }
-        }]
-      },
-      {
-        test: /\.vue$/,
-        use: ["vue-loader"]
-      }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(["dist"]),
+      new MiniCssExtractPlugin(),
+      new HtmlWebPackPlugin({
+        template: "src/index.html",
+        filename: "index.html"
+      }),
+      new VueLoaderPlugin()
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(["dist"]),
-    new HtmlWebPackPlugin({
-      template: "src/index.html",
-      filename: "index.html"
-    }),
-    new VueLoaderPlugin()
-  ]
+  }
 };
